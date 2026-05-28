2ef506a5cc2497846da6056200c4233a2a943152
Jakub Kočnar <166435435+Kebluk@users.noreply.github.com>
2026-05-28 08:35:46 +0200

    Digital read and write - RGB-Btn-Example.c

--- lib/NotArduino/NotArduino.c
+++ lib/NotArduino/NotArduino.c
@@ -17,12 +17,12 @@ void digitalWrite(NotArduinoPin pin, NotArduinoDigitalValue digitalValue) {
     int port = pin / 32;
     int pinInPort = pin % 32;
 
-    //(value)
-
+    (digitalValue == HIGH) ? (GPIOS[port]->PDOR |= (1 << pinInPort)) : (GPIOS[port]->PDOR &= ~(1 << pinInPort));
 }
 
 int digitalRead(NotArduinoPin pin) {
     int port = pin / 32;
     int pinInPort = pin % 32;
 
+    return (GPIOS[port]->PDIR & (1 << pinInPort)) ? HIGH : LOW;
 }
