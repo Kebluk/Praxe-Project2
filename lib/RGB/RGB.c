71f3c2d8fbcd9f2e35c2bb799dc0b832a62b084c
Jakub Kočnar <166435435+Kebluk@users.noreply.github.com>
2026-05-28 13:22:14 +0200

    Extended and documented RGB

--- lib/RGB/RGB.c
+++ lib/RGB/RGB.c
@@ -1,39 +1,38 @@
 #include "NotArduino.h"
 #include "RGB.h"
-#include "MKL25Z4.h"
 
 void RGB_init(void) {
-    /*                      Configure TPM0 for PWM
-    TPM0->CONTROLS[0].CnSC |= 0b01010; // Edge-aligned PWM, high-true pulses
-    TPM0->CONTROLS[1].CnSC |= 0b01010; // Edge-aligned PWM, high-true pulses
-    TPM0->CONTROLS[2].CnSC |= 0b01010; // Edge-aligned PWM, high-true pulses
-    */
-
-    pinMode(PB18, OUTPUT); // Red
-    pinMode(PB19, OUTPUT); // Green
-    pinMode(PD1, OUTPUT);  // Blue
-    RGB_Set(OFF, OFF, OFF); // Initialize RGB to off
-    }
-
-void RGB_Set(NotArduinoSwich red, NotArduinoSwich green, NotArduinoSwich blue) {
-    // Implementation for setting RGB values with PWM
-    digitalWrite(PB18, !red);   // Set red value
-    digitalWrite(PB19, !green); // Set green value
-    digitalWrite(PD1, !blue);   // Set blue value
+    RGB_Set(OFF); // Initialize RGB to off
 }
 
-void RGB_SetBrightness(RGBColor color, uint8_t value) {
+void RGB_Set(NotArduinoSwich state) {
+    RGB_SetColor(RGB_RED, state);
+    RGB_SetColor(RGB_GREEN, state);
+    RGB_SetColor(RGB_BLUE, state);
+}
+
+void RGB_SetColor(RGBColor color, NotArduinoSwich state) {
+    analogWrite((color == RGB_RED) ? PB18 : (color == RGB_GREEN) ? PB19 : PD1, (state == ON) ? 255 : 0);     // Set red channel
+}
+
+void RGB_AnalogWrite(uint8_t red, uint8_t green, uint8_t blue) {
+    // Implementation for setting RGB values with PWM
+    analogWrite(PB18, red);   // Set red value
+    analogWrite(PB19, green); // Set green value
+    analogWrite(PD1, blue);   // Set blue value
+}
+
+void RGB_SetColorBrightness(RGBColor color, uint8_t value) {
     // Implementation for setting brightness of a specific color using PWM
     switch (color) {
         case RGB_RED:
-            TPM0->CONTROLS[0].CnV = value; // Set red brightness
+            analogWrite(PB18, value); // Set red brightness
             break;
         case RGB_GREEN:
-            TPM0->CONTROLS[1].CnV = value; // Set green brightness
+            analogWrite(PB19, value); // Set green brightness
             break;
         case RGB_BLUE:
-            TPM0->CONTROLS[2].CnV = value; // Set blue brightness
+            analogWrite(PD1, value); // Set blue brightness
             break;
     }
-
 }
--- lib/RGB/RGB.h
+++ lib/RGB/RGB.h
@@ -1,12 +1,37 @@
 #pragma once
 #include "MKL25Z4.h"
 
-typedef enum {
+typedef enum
+{
     RGB_RED = 0,
     RGB_GREEN = 1,
     RGB_BLUE = 2
 } RGBColor;
 
+/** @brief Initialize the RGB LED */
 void RGB_init(void);
-void RGB_Set(NotArduinoSwich red, NotArduinoSwich green, NotArduinoSwich blue);
-void RGB_SetBrightness(RGBColor color, uint8_t value);
+
+/**
+ * @brief Set the state of the RGB LED
+ * @param state The state to set the RGB LED to
+ */
+void RGB_Set(NotArduinoSwich state);
+
+/** @brief Set the state of a specific color channel
+ * @param color The color channel to set
+ * @param state The state to set the color channel to
+ */
+void RGB_SetColor(RGBColor color, NotArduinoSwich state);
+
+/** @brief Set the analog values for each color channel
+ * @param red The brightness of the red channel (0-255)
+ * @param green The brightness of the green channel (0-255)
+ * @param blue The brightness of the blue channel (0-255)
+ */
+void RGB_AnalogWrite(uint8_t red, uint8_t green, uint8_t blue);
+
+/** @brief Set the brightness of a specific color channel
+ * @param color The color channel to set the brightness for
+ * @param value The brightness value (0-255)
+ */
+void RGB_SetColorBrightness(RGBColor color, uint8_t value);
